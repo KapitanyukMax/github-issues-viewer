68648af112de988f1d0bd16833f36befc4b216a2
KapitanyukMax <maxkapitaniuk@gmail.com>
2025-04-07 14:54:36 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    plugins: {
+      "unused-imports": eslintPluginUnusedImports,
+    },
+    rules: {
+      // Disable the default no-unused-vars rule to avoid conflict
+      "no-unused-vars": "off",
+
+      // Enable unused-imports plugin rules
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
